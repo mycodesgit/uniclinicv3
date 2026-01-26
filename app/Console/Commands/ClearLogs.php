31aa4c5095259e8c80c25157ee019c792b806614
mycodesgit <lowexian@gmail.com>
2026-01-26 14:15:42 +0800

    Update

---
+++ app/Console/Commands/ClearLogs.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearLogs extends Command
+{
+    protected $signature = 'logs:clear';
+    protected $description = 'Clear all log files in storage/logs';
+
+    public function handle()
+    {
+        $logPath = storage_path('logs');
+
+        $files = File::files($logPath);
+
+        foreach ($files as $file) {
+            File::put($file->getPathname(), ''); // Truncate each log file
+        }
+
+        $this->info('Logs have been cleared!');
+    }
+}
